8779583f8dd67f82d92eb972c8bccab8084241a2
Prathinav-kV <68032161+Prathinav-kV@users.noreply.github.com>
2026-06-28 03:31:49 +0000

    added the fastest solution

--- Arrays and Hashing/twoSum.py
+++ Arrays and Hashing/twoSum.py
@@ -8,3 +8,17 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if (a + nums[j] == target):
                     return [i,j]
+
+#FASTEST SOLUTION
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pos = dict();
+
+        for i, num in enumerate(nums):
+            diff = target - num;
+            if diff in pos:
+                return [pos[diff], i]
+
+            pos[num] = i;
+
+        return [];
